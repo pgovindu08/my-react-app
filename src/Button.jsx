441ed4bf2d874aa458ec80c12e4a773052543c3f
pgovindu08 <pssg2103@gmail.com>
2025-12-05 02:20:34 -0600

    third commit

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,15 +1,5 @@
 function Button(){
-    
-    const styles = {
-        backgroundColor: "cyan",
-        color: "black",
-        padding: "10px 20px",
-        bordeRadius: "5px",
-        border: "none",
-        cursor: "pointer",
-    }
-
-    return(<button style = {styles}>Click Me</button>)
+    return(<button className="button">Click Me</button>)
 }
 
 export default Button
